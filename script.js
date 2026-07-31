14f8b7f5e7002bfcfa83c0468a9dde8b7a033356
Kerols Nakhla <nakhlakero@gmail.com>
2026-07-31 15:35:41 +0300

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,17 @@
+// Portfolio Website
+
+console.log("Portfolio Loaded Successfully 🚀");
+
+// Current Year
+
+const year = new Date().getFullYear();
+
+console.log("Year:", year);
+
+// Smooth loading
+
+window.onload = function(){
+
+    document.body.style.opacity = "1";
+
+}
